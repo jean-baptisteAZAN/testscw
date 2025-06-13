8a3887a5d0d1e6ac8c22426062631ae28d469e0e
Jean-Baptiste Azan <azan.jean-baptiste@epitech.eu>
2025-06-13 10:39:53 +0200

    feat: anims

---
+++ testSCW/testSCWApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSCWApp.swift
+//  testSCW
+//
+//  Created by Jean-Baptiste  Azan on 13/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct testSCWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testSCWTests/testSCWTests.swift
@@ -0,0 +1,17 @@
+//
+//  testSCWTests.swift
+//  testSCWTests
+//
+//  Created by Jean-Baptiste  Azan on 13/06/2025.
+//
+
+import Testing
+@testable import testSCW
+
+struct testSCWTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
